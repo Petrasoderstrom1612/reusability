a6cc35a420c3a940d186bd0178d3773cc8307a99
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-07-31 14:27:12 +0200

    added Marquee for CSS purposes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Button from './components/Button'
+import Marquee from './components/Marquee'
 
 
 function App() {
 
   return (
     <>
+      {/* <Marquee text="🧛‍♀️ Welcome to Horrorville 🧛‍♀️" /> */}
       <Button>hello</Button>
     </>
   )
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = (props) => {
+const Button = ({children}) => {
   return (
     <>
-    <button>{props.children}</button>
+    <button>{children}</button>
     </>
   )
 }
