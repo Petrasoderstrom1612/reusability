147868b30d738a8a4616fb11021271bf054f913d
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-07-31 15:00:56 +0200

    installed react-icons --save and added an icon as a child

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import Button from './components/Button'
 import Marquee from './components/Marquee'
+import { FaMoneyBill } from "react-icons/fa";
 
 
 function App() {
 
   return (
     <>
+      <Button>
+        <FaMoneyBill />
+        Buy now!
+      </Button>
       {/* <Marquee text="🧛‍♀️ Welcome to Horrorville 🧛‍♀️" /> */}
-      <Button>hello</Button>
     </>
   )
 }
