f73da01de98af23435ae8404b4eb97ebfa326012
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-08-01 16:02:59 +0200

    two classes combined

--- src/components/Google.jsx
+++ src/components/Google.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Google = ({children, ...rest}) => {
-  let sizing = ""
+  //let sizing = "" otherwise place instead of rest.size. This could be used if classNames differ
+  // if (rest.size === "sm") sizing = "sm"
+  // if (rest.size === "lg") sizing = "lg" 
 
-  if (rest.size === "sm") sizing = "button-small"
-  if (rest.size === "lg") sizing = "button-large" 
 
   return (
-    <>
-    <button {...rest} className={sizing}>{children}</button> {/* the position of {...rest} decides whether the green class will be overriten or will override*/}
+    <>                {/*you can do rest.size or add size after children as destructured in arguments and then just size in this component without . */}
+    <button {...rest} className={`${rest.size} ${rest.className}`}>{children}</button> {/* the position of {...rest} decides whether the green class will be overriten or will override*/}
     </>
   )
 }
