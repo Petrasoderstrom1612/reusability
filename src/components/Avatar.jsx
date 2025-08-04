9559a8bf28ba9210b6cf66aa50492a499bcbb252
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-08-04 08:44:33 +0200

    placed the randomly generated colors in a state on the child so they do not change on renders

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,20 +2,23 @@ import React from "react"
 import { IoPersonSharp } from "react-icons/io5"
 
 export default function Avatar(props) {
+    console.log(props)
+
     let colors = ["navy", "pink", "red", "blue", "green"]
-    
     const generateRandom = () => {
         return colors[Math.floor(Math.random() * colors.length)]
     }
-    console.log(props)
-    const withChild = generateRandom()
-    const anonymous = generateRandom()
+
+    const [avatarColor, setAvatarColor] = React.useState(() => generateRandom())
+    const [anonymousColor, setAnonymousColor] = React.useState(() => generateRandom())
+    
+
     
     return (
         <>
         {props.src && <div className={props.className}><img src={props.src} alt={props.alt} {...props}/></div>}
-        {props.children && <div className={`avatar avatar-letters ${withChild}`}>{props.children}</div>}
-        {!props.src && !props.children && <div className={`avatar avatar-icon ${anonymous}`}><IoPersonSharp /></div>}
+        {props.children && <div className={`avatar avatar-letters ${avatarColor}`}>{props.children}</div>}
+        {!props.src && !props.children && <div className={`avatar avatar-icon ${anonymousColor}`}><IoPersonSharp /></div>}
         </>
     )
 }
